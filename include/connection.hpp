efec1477c63d536967bdddeed83f2c29514ccd0e
felippe <mendonca.felippe@gmail.com>
2017-03-20 19:20:02 -0300

    wait_event funcionality implemented

--- include/connection.hpp
+++ include/connection.hpp
@@ -4,6 +4,7 @@
 #include <SimpleAmqpClient/SimpleAmqpClient.h>
 #include <algorithm>
 #include <chrono>
+#include <functional>
 #include <string>
 #include <vector>
 #include "helpers.hpp"
@@ -13,18 +14,25 @@ namespace is {
 using namespace AmqpClient;
 using namespace std::chrono;
 
+struct QueueInfo {
+  const std::string name;
+  const std::string tag;
+
+  QueueInfo(std::string const& name, std::string const& tag) : name(name), tag(tag) {}
+};
+
 struct Connection {
   Channel::ptr_t channel;
-  const std::string exchange;
 
   Connection(std::string const& uri, std::string const& exchange = "data") : Connection(make_channel(uri), exchange) {}
 
-  Connection(Channel::ptr_t channel, std::string const& exchange = "data") : channel(channel), exchange(exchange) {
+  Connection(Channel::ptr_t channel, std::string const& exchange = "data") : channel(channel) {
     // passive durable auto_delete
     channel->DeclareExchange(exchange, Channel::EXCHANGE_TYPE_TOPIC, false, false, false);
   }
 
-  bool publish(std::string const& topic, BasicMessage::ptr_t message, bool mandatory = false) {
+  bool publish(std::string const& topic, BasicMessage::ptr_t message, std::string const& exchange = "data",
+               bool mandatory = false) {
     try {
       if (!message->TimestampIsSet()) {
         set_timestamp(message);
@@ -36,12 +44,15 @@ struct Connection {
     return true;
   }
 
-  std::string subscribe(std::string const& topic, int queue_size = 1) {
+  void unsubscribe(QueueInfo const& info) { channel->DeleteQueue(info.name); }
+
+  QueueInfo subscribe(std::string const& topic, std::string const& exchange = "data", int queue_size = 1) {
     std::vector<std::string> topics{topic};
-    return subscribe(topics, queue_size);
+    return subscribe(topics, exchange, queue_size);
   }
 
-  std::string subscribe(std::vector<std::string> const& topics, int queue_size = 0) {
+  QueueInfo subscribe(std::vector<std::string> const& topics, std::string const& exchange = "data",
+                      int queue_size = 0) {
     // queue_name, passive, durable, exclusive, auto_delete
     std::string queue;
     if (queue_size) {
@@ -56,24 +67,27 @@ struct Connection {
     }
 
     // no_local, no_ack, exclusive, message_prefetch_count
-    return channel->BasicConsume(queue, "", true, true, true);
+    auto tag = channel->BasicConsume(queue, "", true, true, true);
+
+    QueueInfo info(queue, tag);
+    return info;
   }
 
-  Envelope::ptr_t consume(std::string const& tag) { return channel->BasicConsumeMessage(tag); }
+  Envelope::ptr_t consume(QueueInfo const& info) { return channel->BasicConsumeMessage(info.tag); }
 
   template <typename Time>
-  Envelope::ptr_t consume_for(std::string const& tag, Time const& timeout) {
+  Envelope::ptr_t consume_for(QueueInfo const& info, Time const& timeout) {
     int timeout_ms = duration_cast<milliseconds>(timeout).count();
     Envelope::ptr_t envelope;
-    channel->BasicConsumeMessage(tag, envelope, timeout_ms);
+    channel->BasicConsumeMessage(info.tag, envelope, timeout_ms);
     return envelope;
   }
 
-  std::vector<Envelope::ptr_t> consume_sync(std::vector<std::string> const& tags, int64_t period_ms) {
+  std::vector<Envelope::ptr_t> consume_sync(std::vector<QueueInfo> const& infos, int64_t period_ms) {
     std::vector<Envelope::ptr_t> envelopes;
-    envelopes.reserve(tags.size());
-    for (auto& tag : tags) {
-      auto envelope = consume(tag);
+    envelopes.reserve(infos.size());
+    for (auto& info : infos) {
+      auto envelope = consume(info);
       envelopes.emplace_back(envelope);
     }
 
@@ -90,20 +104,19 @@ struct Connection {
       }
 
       auto min_el = std::distance(std::begin(envelopes), minmax.first);
-      envelopes[min_el] = consume(tags[min_el]);
+      envelopes[min_el] = consume(infos[min_el]);
     }
 
     return envelopes;
   }
 
-  std::vector<Envelope::ptr_t> consume_sync(std::string const& tag, std::vector<std::string> topics,
-                                            int64_t period_ms) {
+  std::vector<Envelope::ptr_t> consume_sync(QueueInfo const& info, std::vector<std::string> topics, int64_t period_ms) {
     std::vector<Envelope::ptr_t> envelopes(topics.size());
     std::sort(std::begin(topics), std::end(topics));
 
     int n_unitialized = topics.size();
     while (n_unitialized) {
-      auto envelope = consume(tag);
+      auto envelope = consume(info);
 
       auto it = std::lower_bound(std::begin(topics), std::end(topics), envelope->RoutingKey());
       if (it != std::end(topics)) {
@@ -127,7 +140,7 @@ struct Connection {
         break;
       }
 
-      auto envelope = consume(tag);
+      auto envelope = consume(info);
       auto it = std::lower_bound(std::begin(topics), std::end(topics), envelope->RoutingKey());
       if (it != std::end(topics)) {
         auto el = std::distance(std::begin(topics), it);
@@ -136,6 +149,18 @@ struct Connection {
     }
 
     return envelopes;
+  }
+
+  void wait_event(std::string const& event, std::function<bool(Table)> predicate) {
+    auto events_tag = this->subscribe(event, "amq.rabbitmq.event");
+    while (1) {
+      auto event = this->consume(events_tag);
+      if (event->Message()->HeaderTableIsSet()) {
+        if (predicate(event->Message()->HeaderTable())) {
+          return;
+        }
+      }
+    }
   }
 };
 
