3afe2bff5c577d07ec65857c7b84555ed179de62
felippe <mendonca.felippe@gmail.com>
2017-06-14 22:16:15 -0300

    Created new camera messages.

--- include/msgs/camera.hpp
+++ include/msgs/camera.hpp
@@ -1,11 +1,15 @@
 #ifndef __IS_MSG_CAMERA_HPP__
 #define __IS_MSG_CAMERA_HPP__
 
+#include <boost/optional.hpp>
 #include "../packer.hpp"
+#include "common.hpp"
 
 namespace is {
 namespace msg {
 namespace camera {
+
+using namespace is::msg::common;
 
 struct TheoraPacket {
   bool new_header;
@@ -42,6 +46,45 @@ namespace image_type {
 const ImageType rgb{"rgb"};
 const ImageType gray{"gray"};
 }
+
+struct Exposure {
+  float value;
+  boost::optional<bool> auto_mode;
+  IS_DEFINE_MSG(value, auto_mode);
+};
+
+struct Shutter {
+  boost::optional<float> percent;
+  boost::optional<float> ms;
+  boost::optional<bool> auto_mode;
+  IS_DEFINE_MSG(percent, ms, auto_mode);
+};
+
+struct Gain {
+  boost::optional<float> percent;
+  boost::optional<float> db;
+  boost::optional<bool> auto_mode;
+  IS_DEFINE_MSG(percent, db, auto_mode);
+};
+
+struct WhiteBalance {
+  boost::optional<unsigned int> red;
+  boost::optional<unsigned int> blue;
+  boost::optional<bool> auto_mode;
+  IS_DEFINE_MSG(red, blue, auto_mode);
+};
+
+struct Configuration {
+  boost::optional<Resolution> resolution;
+  boost::optional<SamplingRate> sampling_rate;
+  boost::optional<ImageType> image_type;
+  boost::optional<float> brightness;
+  boost::optional<Exposure> exposure;
+  boost::optional<Shutter> shutter;
+  boost::optional<Gain> gain;
+  boost::optional<WhiteBalance> white_balance;
+  IS_DEFINE_MSG(resolution, sampling_rate, image_type, brightness, exposure, shutter, gain, white_balance);
+};
 
 }  // ::camera
 }  // ::msg
